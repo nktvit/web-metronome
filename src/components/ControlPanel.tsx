a59dd6c67c47be0d23cffbfd7f0d9f265e81ddf1
nktvit <nikita.vitkovsky@icloud.com>
2024-05-21 11:11:15 +0100

    ✏️Improved UI 🧱Improved structure

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,11 +1,17 @@
-import { memo } from 'react';
-import { FaPlay, FaStop } from 'react-icons/fa6';
+import {memo} from 'react';
+import {FaPlay, FaStop} from 'react-icons/fa6';
 
-const ControlPanel = memo(({ isPlaying, togglePlay }: { isPlaying: boolean, togglePlay: () => void }) => {
+const ControlPanel = memo(({isPlaying, togglePlay, className}: {
+    isPlaying: boolean,
+    togglePlay: () => void,
+    className?: string
+}) => {
     return (
-            <button className=" text-lg p-3 rounded-full transition-[background-color] focus:bg-zinc-600 hover:bg-zinc-800" onClick={togglePlay}>
-                {isPlaying ? <FaStop /> : <FaPlay />}
-            </button>
+        <button
+            className={`${className} text-lg p-3 rounded-full transition-[background-color] focus:bg-zinc-600 hover:bg-zinc-800`}
+            onClick={togglePlay}>
+            {isPlaying ? <FaStop/> : <FaPlay/>}
+        </button>
     );
 });
 
